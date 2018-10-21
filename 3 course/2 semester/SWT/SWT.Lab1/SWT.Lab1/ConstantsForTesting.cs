6f737111eccc146d531f4241b03b7a9a7c0f501f
Daniel <danielghazi00@gmail.com>
2018-10-21 11:15:16 +0300

    SWT

---
+++ 3 course/2 semester/SWT/SWT.Lab1/SWT.Lab1/ConstantsForTesting.cs
@@ -0,0 +1,18 @@
+﻿namespace SWT.Lab1
+{
+    static class ConstantsForTesting
+    {
+        public static readonly string ChromeExeDirectory = @"C:\Repositories\ITMO\3 course\2 semester\SWT\Common";
+
+        public static readonly string WebsiteUrl = "https://www.tumblr.com/";
+        public static readonly string Email = "etoktotakoy@mail.ru";
+        public static readonly string Password = "somerandompassword";
+
+        public static readonly string LoginButtonPath = "//button[@id='signup_login_button']";
+        public static readonly string EmailTextFieldPath = "//input[@id='signup_determine_email']";
+        public static readonly string SubmitButtonPath = "//button[@id='signup_forms_submit']";
+        public static readonly string UsePasswordButtonPath = "(//div[@id='signup_magiclink']/child::node())[2]/a";
+        public static readonly string PasswordTextFieldPath = "//input[@id='signup_password']";
+
+    }
+}
---
+++ 3 course/2 semester/SWT/SWT.Lab1/SWT.Lab1/Tests.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+using static SWT.Lab1.ConstantsForTesting;
+
+namespace SWT.Lab1
+{
+    public class Tests
+    {
+        IWebDriver _driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            _driver = new ChromeDriver(ChromeExeDirectory);
+        }
+
+        [Test]
+        public void TumblerFunctionality()
+        {
+            _driver.Url = WebsiteUrl;
+            Login();
+
+
+
+            Thread.Sleep(3000);
+        }
+
+        private void Login()
+        {
+            IWebElement loginButton = _driver.FindElement(By.XPath(LoginButtonPath));
+            loginButton.Click();
+
+            IWebElement emailTextField = _driver.FindElement(By.XPath(EmailTextFieldPath));
+            emailTextField.SendKeys(Email);
+
+            IWebElement confirmEmailButton = _driver.FindElement(By.XPath(SubmitButtonPath));
+            confirmEmailButton.Click();
+
+            Thread.Sleep(1500);
+
+            IWebElement usePasswordButton = _driver.FindElement(By.XPath(UsePasswordButtonPath));
+            usePasswordButton.Click();
+
+            IWebElement passwordTextField = _driver.FindElement(By.XPath(PasswordTextFieldPath));
+            passwordTextField.SendKeys(Password);
+
+            IWebElement confirmPasswordButton = _driver.FindElement(By.XPath(SubmitButtonPath));
+            confirmPasswordButton.Click();
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            _driver.Close();
+        }
+    }
+}
